3cc7445c2286eb872ca9ffefa2b22ab7989bd456
Fabian Fett <fabianfett@apple.com>
2021-12-21 09:53:27 +0100

    Add MetricsTestUtils target (#11)
    * Add MetricsTestUtils target
    
    * code review
    
    * Code format
    
    * Fix CI for 5.0 and 5.1
    
    * Returns for 5.0
    
    Co-authored-by: Konrad `ktoso` Malawski <ktoso@apple.com>

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
     name: "swift-metrics-extras",
     products: [
         .library(name: "SystemMetrics", targets: ["SystemMetrics"]),
+        .library(name: "MetricsTestUtils", targets: ["MetricsTestUtils"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.1.1"),
@@ -27,6 +28,10 @@ let package = Package(
         .target(
             name: "SystemMetrics",
             dependencies: ["CoreMetrics"]
+        ),
+        .target(
+            name: "MetricsTestUtils",
+            dependencies: ["Metrics", "CoreMetrics"]
         ),
         .testTarget(
             name: "SystemMetricsTests",
